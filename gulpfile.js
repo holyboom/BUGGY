35657ac64e45a2df2738f0f91515890fb4a94c50
Theerawee Rangram <holyboom@me.com>
2015-10-07 23:12:47 +0700

    based change

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,1 @@
-// โหลด package "gulp" มาใช้ (บรรทัดนี้ต้องใส่เสมอ)
-var gulp = require('gulp');
- 
-// โหลด package "gulp-ruby-sass" มาใช้ (บรรทัดนี้ต้องใส่เวลาติดตั้ง plugin เสริม)
-var sass = require('gulp-ruby-sass');
- 
-// เพิ่ม sass ให้ทำพร้อมกับ default task
-gulp.task('default', ['sass'], function() {
-    // ระบุว่า default task ทำอะไร (เว้นไว้ก่อน)
-});
+require('./gulp');
